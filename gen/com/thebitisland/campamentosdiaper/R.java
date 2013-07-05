49457fe96556e35a962493bdcd779366c7447298
satanete <alvaro.sperez@gmail.com>
2013-07-05 17:40:13 +0200

    Login activity: - falta implementar botón login - falta logo a tamaño grande, se ve pixelado ahora

--- gen/com/thebitisland/campamentosdiaper/R.java
+++ gen/com/thebitisland/campamentosdiaper/R.java
@@ -13,6 +13,8 @@ public final class R {
     public static final class color {
         public static final int gradient_background_1=0x7f040000;
         public static final int gradient_background_2=0x7f040001;
+        public static final int light_text=0x7f040003;
+        public static final int login_fields=0x7f040002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -27,11 +29,20 @@ public final class R {
     public static final class drawable {
         public static final int gradient=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int nature_background=0x7f020002;
+        public static final int loginbutton_background=0x7f020002;
+        public static final int logo=0x7f020003;
+        public static final int nature_background=0x7f020004;
+        public static final int sun_background=0x7f020005;
+        public static final int textfield_background=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
-        public static final int nature=0x7f090000;
+        public static final int action_settings=0x7f090006;
+        public static final int login_button=0x7f090005;
+        public static final int logo=0x7f090002;
+        public static final int nature_background=0x7f090000;
+        public static final int password_field=0x7f090004;
+        public static final int sun_background=0x7f090001;
+        public static final int user_field=0x7f090003;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -42,7 +53,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
+        public static final int login_button=0x7f060004;
+        public static final int password_field=0x7f060003;
+        public static final int user_field=0x7f060002;
     }
     public static final class style {
         /** 
