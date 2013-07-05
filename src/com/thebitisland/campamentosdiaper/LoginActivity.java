16ce807635219918f9415eb73ac8e20ae1684b1a
satanete <alvaro.sperez@gmail.com>
2013-07-05 18:35:00 +0200

    pequeños toques... y primera ñapa xD

--- src/com/thebitisland/campamentosdiaper/LoginActivity.java
+++ src/com/thebitisland/campamentosdiaper/LoginActivity.java
@@ -2,14 +2,38 @@ package com.thebitisland.campamentosdiaper;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
+	EditText user_field;
+	EditText password_field;
+	Button login_button;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		user_field = (EditText) findViewById(R.id.user_field);
+		password_field = (EditText) findViewById(R.id.password_field);
+		login_button = (Button) findViewById(R.id.login_button);
+		
+		login_button.setOnClickListener(new OnClickListener() {
+			public void onClick(android.view.View arg0) {
+				String user = user_field.getText().toString();
+				String pass = password_field.getText().toString();
+				Log.d("prueba", user);
+				Log.d("prueba", pass);
+				
+			}
+		});
+
+		
 	}
 
 	@Override
