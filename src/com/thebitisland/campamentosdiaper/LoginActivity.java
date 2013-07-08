7adb268b807f9f34689c6b34805d38c4199e787f
satanete <alvaro.sperez@gmail.com>
2013-07-08 23:26:36 +0200

    Ñapa para ajustar display si sale el keyboard

--- src/com/thebitisland/campamentosdiaper/LoginActivity.java
+++ src/com/thebitisland/campamentosdiaper/LoginActivity.java
@@ -5,15 +5,19 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
 import android.view.View.OnTouchListener;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity {
@@ -35,28 +39,35 @@ public class LoginActivity extends Activity {
 		password_field = (EditText) findViewById(R.id.password_field);
 		login_button = (Button) findViewById(R.id.login_button);
 		logo = (ImageView) findViewById(R.id.logo);
-		
-		/* El logo desaparece cuando introducimos usuario/contraseï¿½a */
-		/*user_field.setOnFocusChangeListener(new OnFocusChangeListener() {
-			public void onFocusChange(View arg0, boolean arg1) {
-				if(arg1){
-					logo.setVisibility(View.GONE);
-				}else{
-					logo.setVisibility(View.VISIBLE);
-				}
-			}
-		});
-		password_field.setOnFocusChangeListener(new OnFocusChangeListener() {
-			public void onFocusChange(View arg0, boolean arg1) {
-				if(arg1){
-					logo.setVisibility(View.GONE);
-				}else{
-					logo.setVisibility(View.VISIBLE);
-				}
-			}
-		});/*
-		/* Problemas: El logo no reaparece */
-		
+
+		/* ñapa v1 */
+		/*
+		 * user_field.setOnFocusChangeListener(new OnFocusChangeListener() {
+		 * public void onFocusChange(View arg0, boolean arg1) { if(arg1){
+		 * logo.setVisibility(View.GONE); }else{
+		 * logo.setVisibility(View.VISIBLE); } } });
+		 * password_field.setOnFocusChangeListener(new OnFocusChangeListener() {
+		 * public void onFocusChange(View arg0, boolean arg1) { if(arg1){
+		 * logo.setVisibility(View.GONE); }else{
+		 * logo.setVisibility(View.VISIBLE); } } });
+		 */
+
+		/* Ñapa vFinal (Ojo con ActionBars, puede dar problemas) */
+		final View activityRootView = (View) findViewById(android.R.id.content);
+		activityRootView.getViewTreeObserver().addOnGlobalLayoutListener(
+				new OnGlobalLayoutListener() {
+					public void onGlobalLayout() {
+						int heightDiff = activityRootView.getRootView()
+								.getHeight() - activityRootView.getHeight();
+						if (heightDiff > 100) { // if more than 100 pixels, its
+												// probably a keyboard...
+							logo.setVisibility(View.GONE);
+						} else {
+							logo.setVisibility(View.VISIBLE);
+						}
+					}
+				});
+
 		login_button.setOnClickListener(new OnClickListener() {
 			public void onClick(android.view.View arg0) {
 				String user = user_field.getText().toString();
@@ -67,9 +78,12 @@ public class LoginActivity extends Activity {
 				/* Falta el login en sï¿½ xD */
 				loginOK = true;
 
-				/* El usuario estï¿½ en un campamento? Se puede devolver en la misma query del login? */
+				/*
+				 * El usuario estï¿½ en un campamento? Se puede devolver en la
+				 * misma query del login?
+				 */
 				camp = true;
-				
+
 				/* fin de lo que falta */
 
 				if (loginOK == true && camp == true) {
@@ -77,9 +91,10 @@ public class LoginActivity extends Activity {
 					startActivity(login);
 					finish();
 				} else if (loginOK == true && camp == false) {
-					/*Intent login = new Intent(context, CampsActivity.class);
-					startActivity(login);
-					finish();*/
+					/*
+					 * Intent login = new Intent(context, CampsActivity.class);
+					 * startActivity(login); finish();
+					 */
 				} else {
 					CharSequence text = "Error en usuario/contraseï¿½a!";
 					int duration = Toast.LENGTH_SHORT;
